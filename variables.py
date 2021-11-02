9bf2db2419cb24dac887318ad72c6e03573f4d76
EmsCode82 <emsgithub@gmail.com>
2021-11-01 19:54:23 -0500

    .

---
+++ variables.py
@@ -0,0 +1,13 @@
+name = "Eric Shepard"
+company = "LearnToProgram.tv"
+age = 39
+
+x=y=z=5
+x=x+2
+print (x,y,z)
+
+age, weight, height = 42, 155, 72
+print("Weight:", weight)
+print("Height:", height)
+
+
